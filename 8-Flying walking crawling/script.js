3112f9a261c74e4344644fe7cd76198cc473501e
TravisM74 <117643403+TravisM74@users.noreply.github.com>
2023-06-15 13:31:44 +0300

    next exercise complete
    part of final project done

--- 8-Flying walking crawling/script.js
+++ 8-Flying walking crawling/script.js
@@ -1,8 +1,8 @@
 document.addEventListener('DOMContentLoaded', function(){
     const canvas = document.getElementById('canvas1');
     const ctx = canvas.getContext('2d');
-    canvas.width = 500;
-    canvas.height = 800;
+    canvas.width = 1000;
+    canvas.height = 1000;
     
     class Game{
         constructor(ctx,width,height){
